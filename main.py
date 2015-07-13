690828e9b780852e56e55b8d4ef698ac508aae1c
Cameron Moreau <cameron.cm6@gmail.com>
2015-07-13 11:16:11 -0500

    Added images to main categories

--- main.py
+++ main.py
@@ -18,6 +18,45 @@ STATE_CATEGORY_THIRD = 4
 
 DATA_DIR = "data/Categories"
 
+class Drawables:
+	def AAfilledRoundedRect(surface,rect,color,radius=0.4):
+	    """
+	    AAfilledRoundedRect(surface,rect,color,radius=0.4)
+
+	    surface : destination
+	    rect    : rectangle
+	    color   : rgb or rgba
+	    radius  : 0 <= radius <= 1
+	    """
+
+	    rect         = pygame.Rect(rect)
+	    color        = pygame.Color(*color)
+	    alpha        = color.a
+	    color.a      = 0
+	    pos          = rect.topleft
+	    rect.topleft = 0,0
+	    rectangle    = pygame.Surface(rect.size,pygame.SRCALPHA)
+
+	    circle       = pygame.Surface([min(rect.size)*3]*2, pygame.SRCALPHA)
+	    pygame.draw.ellipse(circle,(0,0,0),circle.get_rect(),0)
+	    circle       = pygame.transform.smoothscale(circle,[int(min(rect.size)*radius)]*2)
+
+	    radius              = rectangle.blit(circle,(0,0))
+	    radius.bottomright  = rect.bottomright
+	    rectangle.blit(circle,radius)
+	    radius.topright     = rect.topright
+	    rectangle.blit(circle,radius)
+	    radius.bottomleft   = rect.bottomleft
+	    rectangle.blit(circle,radius)
+
+	    rectangle.fill((0,0,0),rect.inflate(-radius.w,0))
+	    rectangle.fill((0,0,0),rect.inflate(0,-radius.h))
+
+	    rectangle.fill(color,special_flags=pygame.BLEND_RGBA_MAX)
+	    rectangle.fill((255,255,255,alpha),special_flags=pygame.BLEND_RGBA_MIN)
+
+	    return surface.blit(rectangle,pos)
+
 class App:
 	def __init__(self):
 		pygame.init()
@@ -26,8 +65,8 @@ class App:
 		self.width = pygame.display.Info().current_w
 		self.height = pygame.display.Info().current_h
 		self.screen = pygame.display.set_mode((self.width, self.height), pygame.FULLSCREEN)
-		self.setState(STATE_INTRO)
-		#self.setState(STATE_CATEGORY)
+		#self.setState(STATE_INTRO)
+		self.setState(STATE_CATEGORY)
 
 	def setState(self, state):
 			if(state == STATE_INTRO):
@@ -140,7 +179,15 @@ class CategoryScreen:
 
 		#Basic fonts and other vars
 		fontBack = pygame.font.Font(FONT_PATH, 52)
+		fontTitle = pygame.font.Font(FONT_PATH, 62)
+
 		backButtonPadding = 30
+
+		#Title
+		if(self.category == STATE_CATEGORY):
+			self.titleText = fontTitle.render('Select a cateogry', True, COLOR_WHITE)
+		else: 
+			self.titleText = fontTitle.render(self.category, True, COLOR_WHITE)
 
 		#Back button text
 		self.backButtonText = fontBack.render('Back', True, COLOR_WHITE)
@@ -159,13 +206,23 @@ class CategoryScreen:
 		self.backButton.fill((0, 0, 0))
 
 		if(self.category == STATE_CATEGORY):
-			print(self.getCategories())
+			self.categories = self.getCategories();
+			self.categoryImages = []
+			for i in range(len(self.categories)):
+				name = DATA_DIR + '/' + self.categories[i] + '/splash.jpg'
+				self.categoryImages.append(
+					pygame.image.load(name)
+				)
 
 	def update(self):
 		self.fillBackground()
 		self.screen.blit(self.titleBar, (0, 0))
 		self.screen.blit(self.backButton, (0, 0))
 		self.screen.blit(self.backButtonText, self.backButtonTextBounds)
+		self.screen.blit(self.titleText, (self.backButtonText.get_width() + 50, 5))
+
+		if(self.category == STATE_CATEGORY):
+			self.drawMainCategories()
 
 	def events(self, event):
 		if event.type == pygame.MOUSEBUTTONDOWN:
@@ -183,6 +240,47 @@ class CategoryScreen:
 				currentW += bgWidth
 			currentH += bgHeight
 			currentW = 0
+
+	def drawMainCategories(self):
+		imageWidth = 500
+		imageHeight = 300
+		maxImageWidth = imageWidth + 16
+		maxImageHeight = imageHeight + 16
+
+		padding = 50
+		
+		bounds = pygame.Surface((
+			(maxImageWidth * len(self.categories)) + ((len(self.categories) - 1) * padding), 
+			maxImageHeight
+		), pygame.SRCALPHA)
+
+		boundsBox = bounds.get_rect()
+		boundsBox.center = (self.screenWidth / 2, self.screenHeight / 2)
+		#self.screen.blit(bounds, boundsBox)
+
+		for i in range(len(self.categories)):
+			image = self.categoryImages[i]
+			image = pygame.transform.scale(image, (imageWidth, imageHeight))
+			pos = (
+				(i * maxImageWidth) + 8 + i * padding, 
+				8
+			)
+
+			self.drawImageWithBorder(bounds, image, pos)
+			bounds.blit(image, pos)
+
+		self.screen.blit(bounds, boundsBox)
+
+	def drawImageWithBorder(self, surface, image, pos):
+		padding = 8
+		border = pygame.Rect(
+			(pos[0] - padding, pos[1] - padding), 
+			(image.get_width() + (padding * 2), image.get_height() + (padding * 2))
+		)
+		Drawables.AAfilledRoundedRect(surface, border, COLOR_WHITE, 0.05)
+
+		surface.blit(image, pos)
+
 
 	def getCategories(self):
 		categories = []
